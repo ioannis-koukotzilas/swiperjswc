3ff488071d2fdc4f64ec9f0f89e9c0d6ac2b5938
Ioannis Koukotzilas <73739698+kocmochq@users.noreply.github.com>
2021-04-10 03:00:44 +0300

    Update functions.php

--- functions.php
+++ functions.php
@@ -1,5 +1,19 @@
 <?php //delete it
 
+// Change number of related products output
+function woo_related_products_limit() {
+	global $product;
+	  
+	  $args['posts_per_page'] = 6;
+	  return $args;
+  }
+  add_filter( 'woocommerce_output_related_products_args', 'jk_related_products_args', 20 );
+	function jk_related_products_args( $args ) {
+	  $args['posts_per_page'] = 12; // 12 related products
+	  $args['columns'] = 1; // arranged in 1 column (for swiper)
+	  return $args;
+  }
+
 //SwiperJS Scripts
 add_action( 'wp_enqueue_scripts', 'swiper_scripts_styles', 20 );
 function swiper_scripts_styles() {
